c17210049e27ef9bfd6bf51261b99039f6fab8b2
danilo-sds <danilosouza302@gmail.com>
2023-09-30 15:37:37 -0300

    First commit

---
+++ js/validandoCpf.js
@@ -0,0 +1,56 @@
+export default function eUmCpf(campo){
+    const cpf = campo.value.replace(/\.|-/g, "");
+    if(validaNumeroRepetidos(cpf) || validaPrimeiroNumero(cpf) || validaSegundoNumero(cpf)){
+        campo.setCustomValidity('Esse cpf não é válido');
+    }
+}
+
+function validaNumeroRepetidos(cpf) {
+    const numerosRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+    return numerosRepetidos.includes(cpf)
+}
+
+function validaPrimeiroNumero(cpf) {
+    let soma = 0;
+    let multiplicar = 10;
+    
+    for(let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicar;
+        multiplicar--;
+    }
+    soma = (soma * 10) % 11;
+
+    if(soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+}
+
+function validaSegundoNumero(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if(soma == 10 || soma == 11) {
+        soma = 0
+    }
+    return soma != cpf[10];
+}
